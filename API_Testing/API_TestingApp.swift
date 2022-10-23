2d439e8988e3c35f0cb4324875694942275a90a6
Raditya <raditya.learner@gmail.com>
2022-10-23 11:28:26 +0800

    Initial Commit

---
+++ API_Testing/API_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_TestingApp.swift
+//  API_Testing
+//
+//  Created by CTSS Students on 23/10/22.
+//
+
+import SwiftUI
+
+@main
+struct API_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
